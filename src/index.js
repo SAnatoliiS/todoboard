4cce9735315b826a5420ef87638d74233a4d51b5
Sergeev Anatolii <serg.anatolii.serg@gmail.com>
2019-02-10 10:12:21 +0300

    Hellow World with styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Home from './components/Home/Home';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app = <Home />;
+
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
