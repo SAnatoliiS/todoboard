ccd996b59d6e8ed8618db0fb4caf394648c4fae7
Sergeev Anatolii <serg.anatolii.serg@gmail.com>
2019-02-17 08:43:29 +0300

    fix bug

--- src/reducers/boardsReducer.js
+++ src/reducers/boardsReducer.js
@@ -1,52 +1,52 @@
 import { handleActions } from 'redux-actions';
 import * as boardsActions from '../actions/boardsActions';
 import * as listsActions from '../actions/listsActions';
-import { boardStatuses } from '../config';
+import { statuses } from '../config';
 import {
-  cutItem,
-  changeItemStatus,
-  addItemToParentList
+	cutItem,
+	changeItemStatus,
+	addItemToParentList
 } from '../utils/stateManipulations';
 
 const defaultState = [
-  {
-    id: '1',
-    name: 'Sports',
-    status: 'ACTIVE',
-    listsInBoard: ['1', '2']
-  },
-  {
-    id: '2',
-    name: 'Hobbies',
-    status: 'ACTIVE',
-    listsInBoard: []
-  },
-  {
-    id: '3',
-    name: 'Plans',
-    status: 'ACTIVE',
-    listsInBoard: ['3']
-  }
+	{
+		id: '1',
+		name: 'Sports',
+		status: 'ACTIVE',
+		listsInBoard: ['1', '2']
+	},
+	{
+		id: '2',
+		name: 'Hobbies',
+		status: 'ACTIVE',
+		listsInBoard: []
+	},
+	{
+		id: '3',
+		name: 'Plans',
+		status: 'ACTIVE',
+		listsInBoard: ['3']
+	}
 ];
 
 export default handleActions(
-  {
-    // ADD_BOARD reducer
-    [boardsActions.addBoard](state, { payload: board }) {
-      return [...state, board];
-    },
-    // MOVE_BOARD_TO_RECYCLE reducer
-    [boardsActions.moveBoardToRecycle](state, { payload: boardId }) {
-      return changeItemStatus(state, boardId, boardStatuses.recycle);
-    },
-    // REMOVE_BOARD reducer
-    [boardsActions.removeBoard](state, { payload: boardId }) {
-      return cutItem(state, boardId);
-    },
-    // ADD_LIST reducer
-    [listsActions.addList](state, { payload: list }) {
-      return addItemToParentList(state, 'list', list.boardId, list.id);
-    }
-  },
-  defaultState
+	{
+		// ADD_BOARD reducer
+		[boardsActions.addBoard](state, { payload: board }) {
+			return [...state, board];
+		},
+		// MOVE_BOARD_TO_RECYCLE reducer
+		[boardsActions.moveBoardToRecycle](state, { payload: boardId }) {
+			return changeItemStatus(state, boardId, statuses.boards.recycle);
+		},
+		// REMOVE_BOARD reducer
+		[boardsActions.removeBoard](state, { payload: boardId }) {
+			return cutItem(state, boardId);
+		},
+		// ADD_LIST reducer
+		[listsActions.addList](state, { payload: list }) {
+			return addItemToParentList(state, 'list', list.boardId, list.id);
+		}
+	},
+	defaultState
 );
--- src/reducers/tasksReducer.js
+++ src/reducers/tasksReducer.js
@@ -1,55 +1,50 @@
 import { handleActions } from 'redux-actions';
 import * as tasksActions from '../actions/tasksActions';
-import { statuses } from '../config';
-import {
-  cutItem,
-  changeItemStatus,
-  changeItemProgressStatus
-} from '../utils/stateManipulations';
+import { cutItem, changeItemProgressStatus } from '../utils/stateManipulations';
 
 const defaultState = [
-  {
-    id: '1',
-    text: 'Board',
-    listId: '1',
-    status: 'ACTIVE',
-    progress: 'IN_PROGRESS'
-  },
-  {
-    id: '2',
-    text: 'Fixators',
-    listId: '1',
-    status: 'ACTIVE',
-    progress: 'DONE'
-  },
-  {
-    id: '3',
-    text: 'Gloves',
-    listId: '2',
-    status: 'ACTIVE',
-    progress: 'IN_PROGRESS'
-  }
+	{
+		id: '1',
+		text: 'Board',
+		listId: '1',
+		status: 'ACTIVE',
+		progress: 'IN_PROGRESS'
+	},
+	{
+		id: '2',
+		text: 'Fixators',
+		listId: '1',
+		status: 'ACTIVE',
+		progress: 'DONE'
+	},
+	{
+		id: '3',
+		text: 'Gloves',
+		listId: '2',
+		status: 'ACTIVE',
+		progress: 'IN_PROGRESS'
+	}
 ];
 
 export default handleActions(
-  {
-    // ADD_TASK reducer
-    [tasksActions.addTask](state, { payload: task }) {
-      return [...state, task];
-    },
-    // REMOVE_TASK reducer
-    [tasksActions.removeTask](
-      state,
-      {
-        payload: { taskId }
-      }
-    ) {
-      return cutItem(state, taskId);
-    },
-    // CHANGE_TASK_PROGRESS_STATUS reducer
-    [tasksActions.changeTaskProgressStatus](state, { payload: taskId }) {
-      return changeItemProgressStatus(state, taskId);
-    }
-  },
-  defaultState
+	{
+		// ADD_TASK reducer
+		[tasksActions.addTask](state, { payload: task }) {
+			return [...state, task];
+		},
+		// REMOVE_TASK reducer
+		[tasksActions.removeTask](
+			state,
+			{
+				payload: { taskId }
+			}
+		) {
+			return cutItem(state, taskId);
+		},
+		// CHANGE_TASK_PROGRESS_STATUS reducer
+		[tasksActions.changeTaskProgressStatus](state, { payload: taskId }) {
+			return changeItemProgressStatus(state, taskId);
+		}
+	},
+	defaultState
 );
