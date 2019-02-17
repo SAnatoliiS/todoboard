0da75c8c8ce06795fdbe4ffe2e22cc0b9d7d32e8
Sergeev Anatolii <serg.anatolii.serg@gmail.com>
2019-02-17 20:30:00 +0300

    ability to clear Completed Tasks

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import { addTask } from '../../actions/tasksActions';
 import { progressStatuses } from '../../config';
 import { moveListToRecycle } from '../../actions/listsActions';
+import { clearCompletedTasks } from '../../thunks/listThunks';
 
 const mapStateToProps = (state, ownProps) => ({
 	tasks: getChildren('lists', ownProps.list.id, state.tasks)(state)
@@ -12,7 +13,8 @@ const mapStateToProps = (state, ownProps) => ({
 
 const mapDispatchToProps = dispatch => ({
 	addTask: (text, listId) => dispatch(addTask(text, listId)),
-	moveToRecycle: listId => dispatch(moveListToRecycle(listId))
+	moveToRecycle: listId => dispatch(moveListToRecycle(listId)),
+	clearCompleted: listId => dispatch(clearCompletedTasks(listId))
 });
 
 const filterTasks = (filterValue, tasks) => {
@@ -52,6 +54,9 @@ class RenderList extends React.Component {
 	onRecycleList = listId => () => {
 		this.props.moveToRecycle(listId);
 	};
+	onClearCompleted = listId => () => {
+		this.props.clearCompleted(listId);
+	};
 	render() {
 		const { list, tasks } = this.props;
 		const filteredTasks = filterTasks(this.state.filterValue, tasks);
@@ -75,7 +80,9 @@ class RenderList extends React.Component {
 					<span onClick={this.pickFilter('active')}>Active</span>
 					<span onClick={this.pickFilter('comleted')}>Completed</span>
 				</div>
-				{completedTasks.length > 0 && <div>Clear completed</div>}
+				{completedTasks.length > 0 && (
+					<div onClick={this.onClearCompleted(list.id)}>Clear completed</div>
+				)}
 
 				<hr />
 			</div>
--- src/thunks/listThunks.js
+++ src/thunks/listThunks.js
@@ -1,12 +1,27 @@
 import * as listsActions from '../actions/listsActions';
 import * as tasksActions from '../actions/tasksActions';
+import { progressStatuses } from '../config';
+
+const findTasks = (allTasks, listId) =>
+	allTasks.filter(task => task.listId === listId);
 
 export const removeListTree = listId => (dispatch, getState) => {
 	const state = getState();
-	const tasks = state.tasks.filter(task => task.listId === listId);
-	console.log(tasks);
+	const tasks = findTasks(state.tasks, listId);
 	tasks.forEach(task =>
 		dispatch(tasksActions.removeTask(task.id, task.listId))
 	);
 	dispatch(listsActions.removeList(listId));
+};
+
+export const clearCompletedTasks = listId => (dispatch, getState) => {
+	const state = getState();
+	const tasks = findTasks(state.tasks, listId);
+	const completedTasks = tasks.filter(
+		task => task.progress === progressStatuses.done
+	);
+	console.log(completedTasks);
+	completedTasks.forEach(task =>
+		dispatch(tasksActions.removeTask(task.id, task.listId))
+	);
 };
