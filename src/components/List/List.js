bd35a300fc40d344f53b6f62d6a475920c31e6aa
Sergeev Anatolii <serg.anatolii.serg@gmail.com>
2019-02-17 20:44:20 +0300

    hide tasks controls if no tasks

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -57,11 +57,32 @@ class RenderList extends React.Component {
 	onClearCompleted = listId => () => {
 		this.props.clearCompleted(listId);
 	};
+	RenderListFooter = (list, tasks) => {
+		if (tasks.length === 0) {
+			return null;
+		}
+		const activeTasks = filterTasks('active', tasks);
+		const completedTasks = filterTasks('comleted', tasks);
+
+		return (
+			<div>
+				<div>{`${activeTasks.length} items left`}</div>
+				<div>
+					<span onClick={this.pickFilter('all')}>All</span>
+					<span onClick={this.pickFilter('active')}>Active</span>
+					<span onClick={this.pickFilter('comleted')}>Completed</span>
+				</div>
+				{completedTasks.length > 0 && (
+					<div onClick={this.onClearCompleted(list.id)}>Clear completed</div>
+				)}
+
+				<hr />
+			</div>
+		);
+	};
 	render() {
 		const { list, tasks } = this.props;
 		const filteredTasks = filterTasks(this.state.filterValue, tasks);
-		const activeTasks = filterTasks('active', tasks);
-		const completedTasks = filterTasks('comleted', tasks);
 		return (
 			<div>
 				<div>
@@ -74,17 +95,7 @@ class RenderList extends React.Component {
 				{filteredTasks.map(task => (
 					<Task key={task.id} task={task} />
 				))}
-				<div>{`${activeTasks.length} items left`}</div>
-				<div>
-					<span onClick={this.pickFilter('all')}>All</span>
-					<span onClick={this.pickFilter('active')}>Active</span>
-					<span onClick={this.pickFilter('comleted')}>Completed</span>
-				</div>
-				{completedTasks.length > 0 && (
-					<div onClick={this.onClearCompleted(list.id)}>Clear completed</div>
-				)}
-
-				<hr />
+				{this.RenderListFooter(list, tasks)}
 			</div>
 		);
 	}
