9ac5f7239e151e7cb0d0f716dc4fe7c1fd2aa790
Sergeev Anatolii <serg.anatolii.serg@gmail.com>
2019-06-14 22:33:23 +0300

    Start ListsList styles.

--- src/components/ListsList/ListsList.js
+++ src/components/ListsList/ListsList.js
@@ -6,31 +6,33 @@ import AddListButton from '../AddListButton/AddListButton';
 import NotFoundPage from '../NotFoundPage/NotFoundPage';
 
 const mapStateToProps = (state, props) => {
-  const board = findItem('boards', props.match.params.id)(state);
-  return {
-    board,
-    activeLists:
-      board &&
-      getActiveChildren('boards', props.match.params.id, state.lists)(state),
-    error: !board && 'Board not found'
-  };
+	const board = findItem('boards', props.match.params.id)(state);
+	return {
+		board,
+		activeLists:
+			board &&
+			getActiveChildren('boards', props.match.params.id, state.lists)(state),
+		error: !board && 'Board not found'
+	};
 };
 
 function ListsList({ activeLists, error, board }) {
-  if (error) {
-    return <NotFoundPage message={error} />;
-  }
-  return (
-    <div>
-      <div>{board.name}</div>
-      <div>
-        {activeLists.map(list => (
-          <RenderList key={list.id} list={list} />
-        ))}
-        <AddListButton boardId={board.id} />
-      </div>
-    </div>
-  );
+	if (error) {
+		return <NotFoundPage message={error} />;
+	}
+	return (
+		<div className={'listsList-container'}>
+			<div className={'listsList-header-container'}>
+				<div className={'listsList-header'}>{board.name}</div>
+			</div>
+			<div className={'listsList-body'}>
+				{activeLists.map(list => (
+					<RenderList key={list.id} list={list} />
+				))}
+				<AddListButton boardId={board.id} />
+			</div>
+		</div>
+	);
 }
 
 export default connect(mapStateToProps)(ListsList);
