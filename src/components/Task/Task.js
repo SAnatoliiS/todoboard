beafcd45e3e9f45547b31ff76fe67d8c860fbbbd
Sergeev Anatolii <serg.anatolii.serg@gmail.com>
2019-07-07 11:37:39 +0300

    Task style.

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,51 +1,56 @@
 import React from 'react';
 import { progressStatuses } from '../../config';
 import {
-  changeTaskProgressStatus,
-  removeTask
+	changeTaskProgressStatus,
+	removeTask
 } from '../../actions/tasksActions';
 import { connect } from 'react-redux';
 
 const mapDispatchToProps = dispatch => ({
-  changeTaskProgressStatus: taskId =>
-    dispatch(changeTaskProgressStatus(taskId)),
-  removeTask: (taskId, listId) => dispatch(removeTask(taskId, listId))
+	changeTaskProgressStatus: taskId =>
+		dispatch(changeTaskProgressStatus(taskId)),
+	removeTask: (taskId, listId) => dispatch(removeTask(taskId, listId))
 });
 
 const getTaskProgressIcon = progress => {
-  switch (progress) {
-    case progressStatuses.inProgress:
-      return '○';
-    case progressStatuses.done:
-      return '✓';
-    default:
-      throw new Error(
-        'no such task status. It might be: "ACTIVE", "RECYCLE" or "DONE"'
-      );
-  }
+	switch (progress) {
+		case progressStatuses.inProgress:
+			return '○';
+		case progressStatuses.done:
+			return '✓';
+		default:
+			throw new Error(
+				'no such task status. It might be: "ACTIVE", "RECYCLE" or "DONE"'
+			);
+	}
 };
 
 function Task({
-  task: { text, progress, id, listId },
-  changeTaskProgressStatus,
-  removeTask
+	task: { text, progress, id, listId },
+	changeTaskProgressStatus,
+	removeTask
 }) {
-  const onClick = id => () => {
-    changeTaskProgressStatus(id);
-  };
-  const onRemove = (id, listId) => () => {
-    removeTask(id, listId);
-  };
-  return (
-    <div>
-      <span onClick={onClick(id)}>{getTaskProgressIcon(progress)}</span>
-      {text}
-      <span onClick={onRemove(id, listId)}> x</span>
-    </div>
-  );
+	const onClick = id => () => {
+		changeTaskProgressStatus(id);
+	};
+	const onRemove = (id, listId) => () => {
+		removeTask(id, listId);
+	};
+	return (
+		<div className={'task-container'}>
+			<div className={'task-checkbox-button'} onClick={onClick(id)}>
+				{getTaskProgressIcon(progress)}
+			</div>
+			<div className={'task-title'}>{text}</div>
+			<div className={'task-close-button'} onClick={onRemove(id, listId)}>
+				{' '}
+				x
+			</div>
+		</div>
+	);
 }
 
 export default connect(
-  null,
-  mapDispatchToProps
+	null,
+	mapDispatchToProps
 )(Task);
