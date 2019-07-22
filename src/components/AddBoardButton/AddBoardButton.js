d9900f063e2fca364108dd231191d21214c60c8d
Sergeev Anatolii <serg.anatolii.serg@gmail.com>
2019-07-22 11:29:50 +0300

    Fix AddBoardButton logic.

--- src/components/AddBoardButton/AddBoardButton.js
+++ src/components/AddBoardButton/AddBoardButton.js
@@ -9,46 +9,74 @@ const mapDispatchToProps = dispatch => ({
 const RenderSmallButton = ({ onClick }) => {
 	return (
 		<div className={'board small-button-container'} onClick={onClick}>
-			<div className={'small-button-content'}>Add board</div>
+			<div className={'small-button-content'}>Add board...</div>
 		</div>
 	);
 };
 
-class RenderFullButton extends Component {
+class AddBoardForm extends Component {
 	state = {
 		name: ''
 	};
 
-	onChange = e => {
+	onChange = ({ ...e }) => {
 		const name = e.target.value;
-		this.setState({ name });
+
+		/** Insert "Enter" */
+		if (e.nativeEvent.inputType === 'insertLineBreak') {
+			if (name.trim().length) {
+				this.onSubmit();
+				return;
+			}
+			return;
+		}
+
+		this.setState({
+			name: name.trim().length ? name : ''
+		});
 	};
 
 	onSubmit = e => {
 		e.preventDefault();
+		if (!this.state.name) {
+			return;
+		}
 		this.props.addBoard(this.state.name);
 		this.props.collapseButton();
-		this.setState({ name: '' });
+	};
+
+	onKeyDown = ({ ...e }) => {
+		if (e.key === 'Escape') {
+			this.props.collapseButton();
+		}
 	};
 
 	render() {
 		const { collapseButton } = this.props;
 		return (
-			<div>
-				<div>
-					Creating a board<span onClick={collapseButton}> X</span>
+			<div className={'board add-board-form-container'}>
+				<div className={'add-board-form-header'}>
+					<div className={'add-board-form-header-content'}>
+						Creating a board
+					</div>
+					<div
+						className={'add-board-form-collapse-button'}
+						onClick={collapseButton}
+					>
+						✖
+					</div>
 				</div>
-				<form onSubmit={this.onSubmit}>
-					<label>What shall we call the board?</label>
+				<div>What shall we call the board?</div>
+				<form onSubmit={this.onSubmit} onKeyDown={this.onKeyDown}>
 					<input
 						type="text"
 						value={this.state.name}
 						onChange={this.onChange}
 						placeholder="Your new board's name"
 					/>
-					<input type="submit" value="Create" />
-					<button onClick={collapseButton}>Cancel</button>
 				</form>
+				<button onClick={collapseButton}>Cancel</button>
+				<button onClick={this.onSubmit}>Create</button>
 			</div>
 		);
 	}
@@ -73,7 +101,7 @@ export class AddBoardButton extends Component {
 				return <RenderSmallButton onClick={this.expandButton} />;
 			case 'full':
 				return (
-					<RenderFullButton
+					<AddBoardForm
 						addBoard={this.props.addBoard}
 						collapseButton={this.collapseButton}
 					/>
