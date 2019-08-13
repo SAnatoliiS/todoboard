bf53d0f4cf52fa7c4a33f5ce36d789618a1ad36d
Sergeev Anatolii <serg.anatolii.serg@gmail.com>
2019-08-13 23:00:59 +0300

    Replace DnD logic into ListsList.

--- src/components/ListsList/ListsList.js
+++ src/components/ListsList/ListsList.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { DragDropContext } from 'react-beautiful-dnd';
 import { getActiveChildren, findItem } from '../../selectors/selectors';
 import RenderList from '../List/List';
 import AddListButton from '../AddListButton/AddListButton';
 import NotFoundPage from '../NotFoundPage/NotFoundPage';
+import { replaceTask } from '../../actions/tasksActions';
 
 const mapStateToProps = (state, props) => {
 	const board = findItem('boards', props.match.params.id)(state);
@@ -17,25 +19,50 @@ const mapStateToProps = (state, props) => {
 	};
 };
 
-function ListsList({ activeLists, error, board }) {
+const mapDispatchToProps = dispatch => ({
+	replaceTask: (taskId, newListId, newIndex) =>
+		dispatch(replaceTask(taskId, newListId, newIndex))
+});
+
+const getOnDragEnd = replaceTask => result => {
+	const { destination, source, draggableId } = result;
+
+	if (!destination) {
+		return;
+	}
+	if (
+		destination.droppableId === source.droppableId &&
+		destination.index === source.index
+	) {
+		return;
+	}
+	replaceTask(draggableId, destination.droppableId, destination.index);
+};
+
+const ListsList = ({ activeLists, error, board, replaceTask }) => {
 	if (error) {
 		return <NotFoundPage message={error} />;
 	}
 	return (
-		<div className={'listsList-container'}>
-			<div className={'listsList-header-container'}>
-				<Link className={'listsList-header'} to="/">
-					{board.name}
-				</Link>
+		<DragDropContext onDragEnd={getOnDragEnd(replaceTask)}>
+			<div className={'listsList-container'}>
+				<div className={'listsList-header-container'}>
+					<Link className={'listsList-header'} to="/">
+						{board.name}
+					</Link>
+				</div>
+				<div className={'listsList-body'}>
+					{activeLists.map(list => (
+						<RenderList key={list.id} list={list} />
+					))}
+					<AddListButton boardId={board.id} />
+				</div>
 			</div>
-			<div className={'listsList-body'}>
-				{activeLists.map(list => (
-					<RenderList key={list.id} list={list} />
-				))}
-				<AddListButton boardId={board.id} />
-			</div>
-		</div>
+		</DragDropContext>
 	);
-}
+};
 
-export default connect(mapStateToProps)(ListsList);
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(ListsList);
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,55 +1,27 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { ConnectedRouter } from 'connected-react-router';
-import { DragDropContext } from 'react-beautiful-dnd';
-import { connect } from 'react-redux';
 import { history } from '../store/configureStore';
 import BoardsList from '../components/BoardsList/BoardsList';
 import NotFoundPage from '../components/NotFoundPage/NotFoundPage';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 import ListsList from '../components/ListsList/ListsList';
-import { replaceTask } from '../actions/tasksActions';
-
-const mapDispatchToProps = dispatch => ({
-	replaceTask: (taskId, newListId, newIndex) =>
-		dispatch(replaceTask(taskId, newListId, newIndex))
-});
-
-const getOnDragEnd = replaceTask => result => {
-	const { destination, source, draggableId } = result;
-
-	if (!destination) {
-		return;
-	}
-	if (
-		destination.droppableId === source.droppableId &&
-		destination.index === source.index
-	) {
-		return;
-	}
-	replaceTask(draggableId, destination.droppableId, destination.index);
-};
 
 const AppRouter = ({ replaceTask }) => (
 	<ConnectedRouter history={history}>
-		<DragDropContext onDragEnd={getOnDragEnd(replaceTask)}>
-			<div className={'app-container'}>
-				<Header />
-				<div className={'app-body'}>
-					<Switch>
-						<Route path="/" exact component={BoardsList} />
-						<Route path="/board/:id" component={ListsList} />
-						<Route component={NotFoundPage} />
-					</Switch>
-				</div>
-				<Footer />
+		<div className={'app-container'}>
+			<Header />
+			<div className={'app-body'}>
+				<Switch>
+					<Route path="/" exact component={BoardsList} />
+					<Route path="/board/:id" component={ListsList} />
+					<Route component={NotFoundPage} />
+				</Switch>
 			</div>
-		</DragDropContext>
+			<Footer />
+		</div>
 	</ConnectedRouter>
 );
 
-export default connect(
-	null,
-	mapDispatchToProps
-)(AppRouter);
+export default AppRouter;
